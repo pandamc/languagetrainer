99366bb3f8a558e65e5ae9ebb1f81755ce5eeb7c
june mcpake <junemc@junes-Air-2.lan>
2020-09-30 21:38:13 +0100

    added empty project with folder structure

---
+++ src/main/java/com/junemc/vocabtrainer/VocabtrainerApplication.java
@@ -0,0 +1,13 @@
+package com.junemc.vocabtrainer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocabtrainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocabtrainerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/junemc/vocabtrainer/VocabtrainerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junemc.vocabtrainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocabtrainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
